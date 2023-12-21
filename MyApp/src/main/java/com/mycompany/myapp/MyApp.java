cc6b716f831fbffcd42fabbd649afabc02c67e7d
FongFus <npphus@gmail.com>
2023-12-21 13:55:07 +0700

    'update_code_for_feature-test'

--- MyApp/src/main/java/com/mycompany/myapp/MyApp.java
+++ MyApp/src/main/java/com/mycompany/myapp/MyApp.java
@@ -14,5 +14,8 @@ public class MyApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World 22!");
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
     }
 }
