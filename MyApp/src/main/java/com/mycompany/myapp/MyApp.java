3f4676a19f433d7d1e24ad74b47b9055e5399645
FongFus <npphus@gmail.com>
2023-12-21 14:09:19 +0700

    'update_feature-test'

--- MyApp/src/main/java/com/mycompany/myapp/MyApp.java
+++ MyApp/src/main/java/com/mycompany/myapp/MyApp.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.myapp;
 
 /**
@@ -14,8 +13,8 @@ public class MyApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World 22!");
-        int a=10;
-        int b=5;
-        System.out.println(a+b);
+        int a = 10;
+        int b = 5;
+        System.out.println("Ket qua la: " + (a + b));
     }
 }
